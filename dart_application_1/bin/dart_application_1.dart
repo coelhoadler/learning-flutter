01dae5752d7d2bf322987e053a55b4afbff4a98f
Adler Santos <adlercoelhosantos12@gmail.com>
2025-08-08 19:38:07 -0300

    feat(app): learning built-in types

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -2,12 +2,32 @@ import 'package:dart_application_1/logic.dart' as dart_application_1;
 
 void main(List<String> arguments) {
 
-  int numero1 = 5;
-  int numero2 = 2;
+  // ---> WORKING WITH RECORDS
+  // var pessoa = ('Adler', 'Coelho Santos', 30);
+  // print('Nome: ${pessoa.$1}');
+  // print('Sobrenome: ${pessoa.$2}');
+  // print('Idade: ${pessoa.$3}');
 
-  double numero3 = 10.5;
-  double numero4 = 57.3;
+  final (texto, numero) = dart_application_1.testeRecords(11);
+  print('Resultado: $texto, $numero');
 
-  print('resultado inteiro: ${dart_application_1.multiplicacaoNumeroInteiros(numero1, numero2)}!');
-  print('resultado decimal: ${dart_application_1.multiplicacaoNumerosDecimais(numero3, numero4)}!');
+  // ---> WORKING WITH TEXTS
+
+  // String texto1 = 'Adler';
+  // String texto2 = 'Coelho Santos';
+
+  // print(dart_application_1.concatenarNomes(texto1, texto2));
+  // print('resultado concatenacao: ${dart_application_1.concatenarStrings(texto1, texto2)}!');
+
+
+  // ---> WORKING WITH NUMBERS
+
+  // int numero1 = 5;
+  // int numero2 = 2;
+
+  // double numero3 = 10.5;
+  // double numero4 = 57.3;
+
+  // print('resultado inteiro: ${dart_application_1.multiplicacaoNumeroInteiros(numero1, numero2)}!');
+  // print('resultado decimal: ${dart_application_1.multiplicacaoNumerosDecimais(numero3, numero4)}!');
 }
--- dart_application_1/lib/logic.dart
+++ dart_application_1/lib/logic.dart
@@ -5,3 +5,15 @@ int multiplicacaoNumeroInteiros(int numero1, int numero2) {
 double multiplicacaoNumerosDecimais(double numero1, double numero2) {
   return numero1 * numero2;
 }
+
+String concatenarNomes(String nome, String sobrenome) {
+  return '$nome $sobrenome';
+}
+
+(String, int) testeRecords(int numero) {
+  if (numero % 2 == 0) {
+    return ('par', numero);
+  }
+
+  return ('impar', numero);
+}
